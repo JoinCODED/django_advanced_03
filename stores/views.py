1ad597797b3233d030c2b031267b54f4cc63c79b
Hussein Almutawa <hussein@joincoded.com>
2018-07-11 10:26:53 +0300

    Django Advanced Challenges template

---
+++ stores/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Store
+
+# Create your views here.
+def store_list(request):
+    context = {
+        "stores": Store.objects.all()
+    }
+    return render(request, 'store_list.html', context)
