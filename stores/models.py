a87c279293858fe04617c0a92e00a78aff0af648
Hussein Almutawa <hussein@joincoded.com>
2018-07-23 20:20:06 +0300

    updated master

--- stores/models.py
+++ stores/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
